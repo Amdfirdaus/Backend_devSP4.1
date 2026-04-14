9aa40d3a9f90258d22608c626772764b022722d1
Amdfirdaus <firdaus2006354@gmail.com>
2026-04-15 04:27:43 +0700

    Add files via upload

---
+++ tugaspertemuan5/crud-mahasiswa/models/Mahasiswa.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const mahasiswaSchema = new mongoose.Schema({
+  nama: {
+    type: String,
+    required: true,
+  },
+  nim: {
+    type: Number,
+    required: true,
+  },
+  jurusan: {
+    type: String,
+    required: true,
+  },
+  alamat: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Mahasiswa", mahasiswaSchema);
